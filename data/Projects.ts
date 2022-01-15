d7d3bb33b284f3c46238974a34fd6533cdbb01ba
Konsey <vgbukalders@gmail.com>
2022-01-15 14:09:34 +0200

    added shiftlog to projects

--- data/Projects.ts
+++ data/Projects.ts
@@ -7,7 +7,8 @@ import git from '../public/static/img/git.png';
 const projects = [
 	{
 		title: 'This Website',
-		description: 'A portfolio website to display my resume and projects',
+		description:
+			'My portfolio website meant for displaying information about me as well as showcasing some of my projects',
 		hoverColor: '#f0f0f0',
 		repo_link: 'https://github.com/Konseyy/portfolio',
 		technologies: [
@@ -32,6 +33,31 @@ const projects = [
 				title: 'GraphQL',
 				url: 'https://graphql.org/',
 				colored: true,
+			},
+			{
+				image: typescript,
+				title: 'TypeScript',
+				url: 'https://www.typescriptlang.org/',
+			},
+			{
+				image: git,
+				title: 'Git',
+				url: 'https://git-scm.com/',
+				colored: true,
+			},
+		],
+	},
+	{
+		title: 'Shift Log',
+		description:
+			'A mobile app meant for tracking work shifts. Has the ability to create and import backups as well as generate .csv files',
+		hoverColor: '#f0f0f0',
+		repo_link: 'https://github.com/Konseyy/ShiftLog',
+		technologies: [
+			{
+				image: react,
+				title: 'React Native',
+				url: 'https://nextjs.org/',
 			},
 			{
 				image: typescript,
