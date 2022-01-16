804ba085acc6dad48d3231d0aa726e5bd6b043c8
Konsey <vgbukalders@gmail.com>
2022-01-16 19:54:03 +0200

    redisigned landing page text

--- components/LandingPageInitial.tsx
+++ components/LandingPageInitial.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import styles from './LandingPageInitial.module.css';
 import SectionButton from './SectionButton';
 interface Props {
@@ -6,13 +6,23 @@ interface Props {
 	id: string;
 }
 const LandingPageInitial: FC<Props> = ({ scrollToId, id }) => {
+	const [isMobile, setIsMobile] = useState(false);
+	useEffect(() => {
+		const listen = window.addEventListener('resize', (e) => {
+			if (window.innerWidth < 500) setIsMobile(true);
+			else setIsMobile(false);
+		});
+		return listen;
+	}, []);
 	return (
 		<div id={id} className={styles.landingPage}>
 			<div className={styles.landingPageIntro}>
-				<p>Hi</p>
-				<span id={styles.firstP}>.</span>
+				<p>{isMobile ? 'Hi' : 'Hello'}</p>
+				<p className={styles.notBold}>,</p>
+				{/* <span id={styles.firstP}>.</span>
 				<span id={styles.secondP}>.</span>
-				<span id={styles.thirdP}>.</span>
+				<span id={styles.thirdP}>.</span> */}
+				<span id={styles.spacer}></span>
 			</div>
 			<SectionButton title="About Me" scrollToId={scrollToId} />
 		</div>
