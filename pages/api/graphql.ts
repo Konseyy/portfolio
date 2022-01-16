554dcfcdcb82c017e263520db397882ae9759c8d
Konsey <vgbukalders@gmail.com>
2022-01-16 11:16:41 +0200

    cors?

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -13,16 +13,16 @@ export default async function handler(
 ) {
 	res.setHeader('Access-Control-Allow-Credentials', 'true');
 	res.setHeader(
-		'Access-Control-Allow-Origin',
-		'https://studio.apollographql.com'
-	);
-	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Headers'
 	);
 	res.setHeader(
 		'Access-Control-Allow-Methods',
 		'POST, GET, PUT, PATCH, DELETE, OPTIONS, HEAD'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://www.valdis.me'
 	);
 	const { db } = await connectToDatabase();
 	const typeDefs = gql`
