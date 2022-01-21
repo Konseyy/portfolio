2645c118f7acf0bb21140bafafe0d12116e2fb15
Konsey <vgbukalders@gmail.com>
2022-01-22 01:04:56 +0200

    updated scrollbar hiding

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -31,6 +31,11 @@ const Skills: FC<Props> = ({ id, scrollToId, skills, displayElements = 5 }) => {
 		skills.slice(0, displayElements - 1)
 	);
 	useEffect(() => {
+		document
+			.getElementsByTagName('html')[0]
+			.setAttribute('class', 'noScrollBar');
+	}, []);
+	useEffect(() => {
 		setSlicedSkills(
 			skills.slice(scrollState.startIndex, scrollState.endIndex + 1)
 		);
@@ -99,12 +104,12 @@ const Skills: FC<Props> = ({ id, scrollToId, skills, displayElements = 5 }) => {
 								}}
 								onMouseEnter={() => {
 									const rootHtml = document.getElementsByTagName('html')[0];
-									rootHtml.setAttribute("class","locked");
+									rootHtml.setAttribute('class', 'noScrollBar locked');
 									// rootHtml.style.height = '100%';
 								}}
 								onMouseLeave={() => {
 									const rootHtml = document.getElementsByTagName('html')[0];
-									rootHtml.setAttribute("class","");
+									rootHtml.setAttribute('class', 'noScrollBar');
 									// rootHtml.style.overflow = 'auto';
 									// rootHtml.style.height = 'auto';
 								}}
