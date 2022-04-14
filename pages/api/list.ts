36cd7f763a6c3c19c79e741e02e3a9ca8018ff51
Konsey <vgbukalders@gmail.com>
2022-04-15 01:36:02 +0300

    update cors header config

--- pages/api/list.ts
+++ pages/api/list.ts
@@ -6,10 +6,6 @@ export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
 ) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'POST,OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-	res.setHeader('Access-Control-Max-Age', '3600');
 	if (req.method === 'OPTIONS') {
 		res.status(200).end();
 		return;
